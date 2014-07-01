a5a8e0d0f0e29ca596b48725c66d0cce353a726c
nekosune <kat.swales@nekokittygames.com>
2014-07-01 21:17:27 +0100

    Added needed ruby backend

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,10 @@
 require 'date'
 class DataController < ApplicationController
   def view
+    #@data=Datum.where(:date=>1.day.ago.to_i..Time.now.to_i)
+    @data=Datum.where(1400536800..1400622300)
+    
+
   end
   
   
